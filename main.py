1b0b00ddc43a9cf0f4dee576fd235b677f8f5b2d
KhesehangLimbu <khesehang81@gmail.com>
2025-03-10 15:50:08 +0545

    Asteroids: Draw Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -7,6 +8,7 @@ def main():
     main_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while (True):
         for event in pygame.event.get():
@@ -14,6 +16,7 @@ def main():
                 return
 
         main_surface.fill((0, 0, 0))
+        player.draw(main_surface)
 
         pygame.display.flip()
         dt = (fps.tick(60))/1000
