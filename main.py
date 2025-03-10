60c075c2e3de16075740e98dfdc481aba6088f12
KhesehangLimbu <khesehang81@gmail.com>
2025-03-10 15:37:51 +0545

    Asteroids: FPS

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ from constants import *
 def main():
     pygame.init()
     main_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
 
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        main_surface.fill((255, 255, 0))
+        main_surface.fill((0, 0, 0))
 
         pygame.display.flip()
+        dt = (fps.tick(60))/1000
 
 
 if __name__ == "__main__":
